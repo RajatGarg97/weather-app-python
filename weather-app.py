3c3314bab15ec96601e2afdc940eda1880f9d645
Rajat Garg <rg51096@gmail.com>
2018-01-19 16:15:54 +0530

    app underconstruction

--- weather-app.py
+++ weather-app.py
@@ -2,10 +2,13 @@ import urllib.request
 import json
 from sys import argv
 import pprint
-
-url = "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+print(argv)
+url = "http://samples.openweathermap.org/data/2.5/weather?q="+argv[1]+"&appid=b6907d289e10d714a6e88b30761fae22"
 req = urllib.request.urlopen(url)
 data = req.read()
 parsedData = json.loads(data)
-pprint.pprint(parsedData)
+#pprint.pprint(parsedData)
 
+obtained = parsedData['main'][argv[2]]
+
+pprint.pprint(obtained)
